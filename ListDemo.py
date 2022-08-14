68999a1fab06b26d8ad6dcc1ff2ca35a02b346ea
iqahmad <hi.iqbala@gmail.com>
2022-08-14 16:10:13 -0400

    This is List Demo

--- ListDemo.py
+++ ListDemo.py
@@ -1,8 +1,8 @@
 
 List1 = [8, 9, 10, 3, 5, 6]
 List1.reverse()
-print("This is reverse" , List1)
+print("This is reverse", List1)
 
 List2 = [10, 22, 33, 44, 66, 11, 12, 34]
 List2.sort()
-print("This is sort list" , List2)
+print("This is sort list", List2)
