129cc23ac9e309aad70e0af582953cc331b8e088
iqahmad <hi.iqbala@gmail.com>
2022-08-15 11:12:13 -0400

    This  is list second push

--- ListDemo.py
+++ ListDemo.py
@@ -8,5 +8,5 @@ print("This is reverse", List1)
 List2 = [10, 22, 33, 44, 66, 11, 12, 34, 88, 99]
 List2.sort()
 print("This is sort list", List2)
-print("#################### End of the massage ###############")
+print("#################### End of the massage , This is second Push  ###############")
 
