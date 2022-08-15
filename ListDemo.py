3ef57355d0b5c6ba224d20afb4d078477d93787a
iqahmad <hi.iqbala@gmail.com>
2022-08-14 22:54:01 -0400

    This  is list job3

--- ListDemo.py
+++ ListDemo.py
@@ -3,7 +3,8 @@ List1 = [8, 9, 10, 3, 5, 6]
 List1.reverse()
 print("This is reverse", List1)
 
-List2 = [10, 22, 33, 44, 66, 11, 12, 34]
+List2 = [10, 22, 33, 44, 66, 11, 12, 34, 88, 99]
 List2.sort()
 print("This is sort list", List2)
 print("#################### End of the massage ###############")
+
